6ced3bbcae1898b0cede4d36ed87f4b61a7f4592
Akhmad Herdian <akhmad.herdian@gmail.com>
2020-04-22 17:38:01 +0800

    Show number of notifications

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,9 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <i class="fa fa-bell"></i>
+                                    <i class="fa fa-bell">
+                                        <span class="badge badge-pill badge-secondary">{{ auth()->user()->notifications->count() }}</span>
+                                    </i>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
